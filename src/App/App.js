0e9973a0267d4bb70552091159ce57261b93dd4d
Katy St Sauveur <katyrogowski@yahoo.com>
2020-11-05 18:51:17 -0700

    Add basic render

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'
 import { getLocationWeather } from '../apiFetch.js'
@@ -8,8 +7,8 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      weatherForecast = {},
-      location = ''
+      weatherForecast: {},
+      location: ''
     }
   }
 
@@ -17,6 +16,14 @@ class App extends Component {
     await getLocationWeather(this.state.location)
     .then(data => this.setState({weatherForecast: data}))
     .catch(error => console.log("NOT FETCHING DATA"))
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Under the Weather</h1>
+      </div>
+    )
   }
 }
 
